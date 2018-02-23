519b22c7f6d39be1abdb3acac98e41e733450afd
Weini <wnlscindy@gmail.com>
2018-02-23 00:19:49 -0800

    initial commit - preprocess processes qadataset

---
+++ lstm.py
@@ -0,0 +1,75 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import preprocess
+from torch.utils.data import DataLoader, Dataset
+
+class VQADataset(Dataset):
+	def __init__(self, img_features, img_map, qa_map):
+		self.img_features = img_features
+		self.img_map = img_map
+		self.qa_map = qa_map
+
+	def __getitem__(self, index):
+		#TODO gpu?
+		qa_id, img_id, q_embed, a_embeds = self.a_map[index]
+		
+		return q_embed, a_embeds, self.img_features[qa[img_id]], [1,0,0,0]
+
+	def __len__(self):
+		return len(self.qa_map)
+
+class LSTMModel(nn.Module):
+	def __init__(self, input_size, hidden_dim, n_layers=1, output_dim, batch_size, dropout):
+		super(LSTMModel, self).__init__()
+		#self.drop = nn.Dropout(dropout)
+
+		self.lstm = nn.LSTM(input_size, hidden_dim, n_layers)
+
+		self.linear = nn.Linear(hidden_dim, output_dim)
+
+		#self.hidden = self.init_hidden()?
+
+
+	def init_hidden(self):
+		return autograd.Variable(torch.rand(self.n_layers, self.batch_size, self.hidden_dim))
+
+	def forward(self, inputs):
+		out, h= self.lstm(inputs)
+		out = self.linear(out)
+
+if __name__ == '__main__':
+	json_filename = '/Users/Weini/Dev/git/visual7w-qa-models/visual7w-toolkit/datasets/visual7w-telling/dataset.json'
+	glove_p_filename = '/Users/Weini/Dev/GloVe/glove.6B/word2vec_glove.6B.300d.pkl'
+	qa_map = preprocess.process_qas(json_filename, glove_p_filename)
+
+	# img_map:{img_id: img_feat_idx}
+
+	model = LSTMModel()
+
+	sents = ['What is the color of the cat red', 'what is the color of the cat blue', "What is the woman wearing"]
+	feats = []
+	glove_embed = # load embedding
+	for sent in sents:
+		feats += np.stack([glove_embed[word] for word in sent]),
+	max_seq_len = max([len(each) for each in feats])
+	# pad if same batch; reshape if single example
+	feats = # from glove
+	dataset = MyDataset(feats, labels)
+	data_loader = some_loader(dataset, batch_size, shuffle=True) # torch builtin
+	
+
+	model.train()
+	loss_fn = torch. # loss func
+	optim = torch.optim.Adam(model.state())
+	for batch in data_loader:
+		feats, labels = batch
+		out, h = model.lstm(feats)
+		score = model.linear(out)
+		loss = loss_fn(score, labels)
+		loss.backward()
+		model.linear.backward()
+		model.lstm.backward()
+		optim
---
+++ preprocess.py
@@ -0,0 +1,93 @@
+import json, string, pickle
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.models.keyedvectors import KeyedVectors
+from nltk.tokenize import WordPunctTokenizer
+import numpy as np
+
+WORD_DIM = 300
+
+# glove2word2vec(glove_input_file='/Users/Weini/Dev/GloVe/glove.6B/glove.6B.300d.txt', word2vec_output_file='/Users/Weini/Dev/GloVe/glove.6B/word2vec_glove.6B.300d.txt')
+# glove_model = KeyedVectors.load_word2vec_format("/Users/Weini/Dev/GloVe/glove.6B/word2vec_glove.6B.300d.txt", binary=False)
+# with open('/Users/Weini/Dev/GloVe/glove.6B/word2vec_glove.6B.300d.pkl', 'wb') as f:
+#     pickle.dump(glove_model, f)
+
+def sent2vec(glove_embed, sentence, word_dim):
+	# v = []
+	v = np.array([]).reshape(0, word_dim)
+	for w in sentence:
+		try:
+			v = np.vstack((v, glove_embed[w]))
+			# v.append(glove_embed[w])
+		except KeyError as e:
+			# print(e)
+			# ise = True
+			# qv.append(np.random.rand(WORD_DIM,))
+			pass
+	return v
+
+def process_qas(json_filename, glove_p_filename):
+	with open(glove_p_filename,'rb') as f:
+		glove_embed = pickle.load(f)
+
+	qa_map = []
+	with open(json_filename) as file:
+		json_data = json.load(file)
+		# qa_data = []
+		tokenizer = WordPunctTokenizer()
+		# qa_id_list = []
+		# image_id_list = []
+		for img in json_data['images']:
+			# image_id_list.append(img["image_id"])
+			# if img['split'] == 'train':
+
+			# elif img['split'] == 'val':
+
+			for qapair in img['qa_pairs']:
+				# qa_id, image_id
+				# qa_id_list.append(qapair['qa_id'])
+				q = tokenizer.tokenize(qapair['question'].strip(string.punctuation).lower())
+				answers = [tokenizer.tokenize(qapair['answer'].strip(string.punctuation).lower())]
+				for choice in qapair['multiple_choices']:
+					answers.append(tokenizer.tokenize(choice.strip(string.punctuation).lower()))
+
+				# qv = np.array([]).reshape(WORD_DIM,)
+				qv = sent2vec(glove_embed, q, WORD_DIM)
+				# print("qv", qv.shape)
+				# qa_m = np.array([]).reshape(0, WORD_DIM)
+				av_list = []
+				for ans in answers:
+					av = sent2vec(glove_embed, ans, WORD_DIM)
+					av_list.append(av)
+					# together = np.vstack((qv, ansv))
+					# print("together", together.shape)
+					# qa_m = np.hstack((qa_m, together))	
+					# try:
+					# 	ansv = sent2vec(glove_embed, ans, WORD_DIM)
+					# 	together = np.vstack((qv, ansv))
+					# 	print("together", together.shape)
+					# 	qa_m = np.hstack((qa_m, together))
+					# except:
+					# 	print("error")
+					# 	print(ansv.shape)
+					# 	print(np.vstack((qv, ansv)).shape)
+					# 	print(ans)
+					# 	exit()
+				qa = {'qa_id': qapair['qa_id'],
+						'img_id': img["image_id"],
+						'q_embed': qv,
+						'a_embeds': av_list}
+				qa_map.append(qa)
+	return qa_map
+
+if __name__ == '__main__':
+	json_filename = '/Users/Weini/Dev/git/visual7w-qa-models/visual7w-toolkit/datasets/visual7w-telling/dataset.json'
+	glove_p_filename = '/Users/Weini/Dev/GloVe/glove.6B/word2vec_glove.6B.300d.pkl'
+	print(process_qas(json_filename, glove_p_filename)[0])
+
+	# print('image_id_list len', len(image_id_list))
+	# print('image_id_list min', min(image_id_list))
+	# print('image_id_list max', max(image_id_list))
+	# print('qa_id_list len', len(qa_id_list))
+	# print('qa_id_list min', min(qa_id_list))
+	# print('qa_id_list max', max(qa_id_list))
+
