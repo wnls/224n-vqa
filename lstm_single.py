b077a1a14d168ea1d8a856c5e5d3ccbd9af3c54e
Weini <wnlscindy@gmail.com>
2018-03-11 21:46:26 -0700

    lstm finetune embeddings

---
+++ lstm_single.py
@@ -0,0 +1,313 @@
+import torch
+import torch.cuda
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+# system utils
+import os
+import sys
+from time import time
+# data utils
+import numpy as np
+import h5py
+import pickle
+import json
+# self-defined modules
+import preprocess
+
+# system
+USE_GPU = torch.cuda.is_available()
+# data
+FEAT_DIM = 2048
+WV_DIM = 300
+dtype = torch.FloatTensor
+# training
+LOSS = 'rank'
+n_epoch = 150
+print_every_train = 50
+print_every_val = 500
+batch_size = 1
+lr = 5e-6
+wd = 5e-3
+# Exec related
+LOAD_TRAIN = True
+LOAD_VAL = True
+LOAD_TEST = True
+PERFORM_TRAIN = True
+
+softmax = nn.Softmax(dim=0)
+
+class VQADataset(Dataset):
+	def __init__(self, img_features, qa_map):
+		self.img_features = img_features
+		self.qa_map = qa_map
+
+	def __getitem__(self, index):
+		qa_id = self.qa_map[index]['qa_id']
+		img_id = str(self.qa_map[index]['img_id'])
+		q_embed = self.qa_map[index]['q_embed']
+		a_embeds = self.qa_map[index]['a_embeds']
+
+		"""
+		Shapes of return values:
+		* q_embed: (n_q, 300)
+		* a_embeds: list of 4 items, each of size (n_a, 300); the 1st item is GT
+		* img_feats: (2048,) i.e. 1D vector
+		"""
+		# return np.stack([q_embed for i in range(4)]), np.stack([a_embed for a_embed in a_embeds]), np.tile(np.asarray(self.img_features[img_id]), [4,1]), np.asarray([1,0,0,0]).reshape(4,1)
+		qa_embeds = []
+		l_max = max([a_embed.shape[0] for a_embed in a_embeds])
+		for a_embed in a_embeds:
+			padded_a_embed = np.concatenate([a_embed, np.zeros([l_max-a_embed.shape[0], 300])], 0)
+			qa_embeds.append(np.vstack((q_embed, padded_a_embed)))
+		qa_encode = np.stack(qa_embeds, 0)
+		img_encode = np.tile(np.asarray(self.img_features[img_id]), [4,1])
+		# print('qa_encode shape:', qa_encode.shape)
+		# print('img_encode shape:',img_encode.shape)
+		sys.stdout.flush()
+		return qa_encode, img_encode, np.asarray([1,0,0,0]).reshape(4,1)
+		# return np.tile(q_embed.mean(0), [4,1]), np.stack([a_embed.mean(0) for a_embed in a_embeds]), np.tile(np.asarray(self.img_features[img_id]), [4,1]), np.asarray([1,0,0,0]).reshape(4,1)
+
+	def __len__(self):
+		return len(self.qa_map)
+
+def pad_collate_fn(batch):
+	"""
+	Input: batch: list
+	Output:
+	"""
+	# max_len = max(batch, key=lambda tp: max(tp[0], key=lambda li: ))
+	# max_len = max([max(tp[0], key=lambda a: a.shape[0]) for tp in batch])
+	qa_embeds = [qa_embed for tp in batch for qa_embed in tp[0]]
+	
+	lens = [i.shape[0] for i in qa_embeds]
+
+	indices = np.argsort(lens)[::-1]
+	lens = lens[indices]
+	max_len = lens[0]
+	batch_size = len(batch)
+
+	np.zeros((batch_size, lens))
+
+	# for qa_embeds, img_feats, labels in batch:
+
+	return batch
+
+class LSTMModel(nn.Module):
+	def __init__(self, visual_dim, lang_dim, hidden_dim, out_dim=1, mlp_dims=[], n_layers=1, dropout=0):
+		super(LSTMModel, self).__init__()
+		#self.drop = nn.Dropout(dropout)
+
+		self.lstm = nn.LSTM(lang_dim, hidden_dim, n_layers)
+		self.visual_dim = visual_dim
+		self.lang_dim = lang_dim
+		self.hidden_dim = hidden_dim
+		self.n_layers = n_layers
+		self.batch_size = 4 # TODO: hard coded
+		self.hidden = self.init_hidden()
+
+		layers = []
+		dims = [hidden_dim+visual_dim] + mlp_dims + [out_dim]
+		for i in range(len(dims)-1):
+			layers += nn.Linear(dims[i], dims[i+1]),
+			if i != len(dims)-2:
+				# i.e. no ReLU at the last layer
+				layers += nn.ReLU(),
+		self.mlp = nn.Sequential(*layers)
+
+	def init_hidden(self):
+		#TODO batch size
+		return (Variable(torch.rand(self.n_layers, self.batch_size, self.hidden_dim)),
+			Variable(torch.rand(self.n_layers, self.batch_size, self.hidden_dim)))
+
+	def forward(self, lang_input, img_input):
+		lang_input = torch.transpose(lang_input, 0, 1)
+		self.hidden = self.init_hidden()
+		out, (h_t, c_t) = self.lstm(lang_input, self.hidden) # h_t: (1, batch (i.e. 4*real_batch), hidden_size)
+		h_t = h_t.squeeze(0)
+		assert(h_t.size(0) == img_input.size(0)), "size mismatch: h_t: {} / img_input: {}".format(h_t.size(), img_input.size())
+		mlp_input = torch.cat([h_t, img_input], 1)
+		out = self.mlp(mlp_input) # batch * 1
+		return out
+
+
+def train(model, optim, loader):
+	model.train()
+
+	top1_cnt = 0
+	total_cnt = 0
+	t_start = time()
+	for i,batch in enumerate(loader):
+		if i and i%print_every_train==0:
+			avg_loss = sum(stats['train_loss'][-print_every_train:])/print_every_train
+			avg_time = (time()-t_start)/print_every_train
+			cur_acc = top1_cnt/total_cnt if total_cnt else -1
+			print('{:d}-{:d}: avg_loss={:f} / avg_time={:f}s / cur_acc={:f}'.format(i-print_every_train, i, avg_loss, avg_time, cur_acc))
+			sys.stdout.flush()
+			t_start = time()
+
+		# unroll a batch
+		qa_embeds, img_feats, gt = batch
+		# Variable for autograd
+		qa_embeds_var = Variable(qa_embeds.view(qa_embeds.size(0)*qa_embeds.size(1), qa_embeds.size(2), qa_embeds.size(3))).type(dtype) # Variable(q_embed.mean(1)).type(dtype)
+		img_feats_var = Variable(img_feats.view(img_feats.size(0)*img_feats.size(1), img_feats.size(2))).type(dtype)
+		gt_var = Variable(gt.view(gt.size(0)*gt.size(1), gt.size(2))).type(dtype)
+		if USE_GPU:
+			gt_var = gt_var.cuda()
+
+		#TODO padding
+		
+		# forward
+		out = model(qa_embeds_var, img_feats_var)
+		if LOSS == 'BCE':
+			# gt_var: 4 * 1
+			# out: 4 * 1
+			loss = loss_fn(out, gt_var)
+		elif LOSS == 'rank':
+			out = softmax(out)
+			y_rank = Variable(torch.Tensor([1,1,1]))
+			if USE_GPU:
+				y_rank = y_rank.cuda()
+			print('pos:', out[0].expand(3))
+			print('neg:', out[1:])
+			print('y_rank:', y_rank)
+			loss = loss_fn(out[0].expand(3), out[1:], y_rank)
+			print(loss)
+		_, idx = out.view(-1).sort(descending=True)
+		# update stats
+		if idx.data[0]==0:
+			top1_cnt += 1 # NOTE: batch size hard coded as 1
+		total_cnt += 1
+		stats['train_loss'].append(loss.data[0])
+		# backward
+		optim.zero_grad()
+		loss.backward()
+		optim.step()
+	acc = top1_cnt/total_cnt if total_cnt else -1
+	stats['train_acc'].append(acc)
+	print("train top@1 accuracy:", acc)
+
+
+def eval(model, loader, update_stats=False, save=''):
+	model.eval()
+
+	top1_cnt = 0
+	total_cnt = 0
+	top1_result = []
+	t_start = time()
+	for i,batch in enumerate(loader):
+		if i and i%print_every_val==0:
+			avg_time = (time()-t_start)/print_every_val
+			cur_acc = top1_cnt/total_cnt if total_cnt else -1
+			print('{:d}-{:d}: avg_time={:f}s / cur_acc={:f}'.format(i-print_every_val, i, avg_time, cur_acc))
+			sys.stdout.flush()
+			t_start = time()
+
+		# unroll a batch
+		qa_embeds, img_feats, gt = batch
+		# Variable for autograd
+		qa_embeds_var = Variable(qa_embeds.view(qa_embeds.size(0)*qa_embeds.size(1), qa_embeds.size(2), qa_embeds.size(3))).type(dtype) # Variable(q_embed.mean(1)).type(dtype)
+		img_feats_var = Variable(img_feats.view(img_feats.size(0)*img_feats.size(1), img_feats.size(2))).type(dtype)
+		gt_var = Variable(gt.view(gt.size(0)*gt.size(1), gt.size(2))).type(dtype)
+		if USE_GPU:
+			gt_var = gt_var.cuda()
+
+		#TODO padding
+		
+		# forward
+		out = model(qa_embeds_var, img_feats_var)
+		# gt_var: 4 * 1
+		# out: 4 * 1
+		loss = loss_fn(out, gt_var)
+		_, idx = out.view(-1).sort(descending=True)
+		# update stats
+		if idx.data[0]==0:
+			top1_cnt += 1 # NOTE: batch size hard coded as 1
+		total_cnt += 1
+		top1_result.append(idx.data[0] == 0)
+	acc = top1_cnt/total_cnt if total_cnt else -1
+	if update_stats:
+		stats['val_acc'].append(acc)
+	print("val top@1 accuracy:", acc)
+	if save:
+		with open(save, 'wb') as fout:
+			np.save(fout, np.concatenate(top1_result))
+	return acc
+
+
+if __name__ == '__main__':
+	# input files
+	json_filename_format = './data/visual7w-telling_{:s}.json'
+	img_feats_fname = './data/resnet101_avgpool.h5'
+	glove_p_filename = './data/word2vec_glove.6B.300d.pkl'
+	saved_qa_map_format = './data/qa_cache_{:s}.pkl'
+	pretrained_path = './checkpoints/lr0.000005_wd0.005000_bts64.pt'
+	use_pretrain = False
+	# output files
+	out_dir = './checkpoints'
+	if not os.path.exists(out_dir):
+		os.mkdir(out_dir)
+	file_format = os.path.join(out_dir, 'lr{:f}_wd{:f}_bts{:d}'.format(lr, wd, batch_size))
+	log_file = file_format + '.json'
+	checkpoint = file_format + '.pt'
+
+	# img_feat # TODO: split img features to be efficient
+        # NOTE: make a copy of the data in img_feats so that we can release the h5py file object
+	with h5py.File(img_feats_fname) as handle: 
+		img_feats = {key:np.asarray(handle[key]) for key in handle}
+	print('Finish loading img_feats.') # NOTE: indicating the file has been released
+
+	if LOAD_TRAIN:
+		# train
+		train_qa_map = pickle.load(open(saved_qa_map_format.format('train'), 'rb')) if os.path.exists(saved_qa_map_format.format('train')) \
+			else preprocess.process_qas(json_filename_format.format('train'), glove_p_filename, save=saved_qa_map_format.format('train'))
+		train_loader = DataLoader(VQADataset(img_feats, train_qa_map), batch_size=batch_size, shuffle=True) #, collate_fn=pad_collate_fn)
+	if LOAD_VAL:
+		# val
+		val_qa_map = pickle.load(open(saved_qa_map_format.format('val'), 'rb')) if os.path.exists(saved_qa_map_format.format('val')) \
+			else preprocess.process_qas(json_filename_format.format('val'), glove_p_filename, save=saved_qa_map_format.format('val'))
+		val_loader = DataLoader(VQADataset(img_feats, val_qa_map), batch_size=1, shuffle=False)
+	if LOAD_TEST:
+		# test
+		test_qa_map = pickle.load(open(saved_qa_map_format.format('test'), 'rb')) if os.path.exists(saved_qa_map_format.format('test')) \
+			else preprocess.process_qas(json_filename_format.format('test'), glove_p_filename, save=saved_qa_map_format.format('test'))
+		test_loader = DataLoader(VQADataset(img_feats, test_qa_map), batch_size=1, shuffle=False)
+
+
+	model = LSTMModel(visual_dim=FEAT_DIM, lang_dim=WV_DIM, hidden_dim=WV_DIM, out_dim=1, mlp_dims=[1024, 512, 512])
+	if LOSS == 'BCE':
+		loss_fn = torch.nn.BCEWithLogitsLoss()
+	elif LOSS == 'rank':
+		loss_fn = torch.nn.MarginRankingLoss()
+	optim = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+	if use_pretrain and pretrained_path:
+		pretrained = torch.load(pretrained_path)
+		model.load_state_dict(pretrained['model'])
+		optim.load_state_dict(pretrained['optim'])
+	if USE_GPU:
+		print("Use GPU")
+		model = model.cuda()
+		loss_fn = loss_fn.cuda()
+	else:
+		print("Use CPU")
+
+	if PERFORM_TRAIN:
+		best_acc = 0
+		stats = {'train_loss':[], 'train_acc':[], 'val_acc':[]}
+		for e in range(n_epoch):
+			print("\n\n==== Epoch {:d} ====".format(e+1))
+			train(model, optim, train_loader)
+			val_acc = eval(model, val_loader, update_stats=True)
+	
+			with open(log_file, "w") as handle:
+				json.dump(stats, handle)
+			
+			if val_acc > best_acc:
+				best_acc = val_acc
+				torch.save({'model': model.state_dict(), 'optim': optim.state_dict()}, checkpoint)
+
+	# Evaluate on test set
+	print("Evaluating on test set...")
+	eval(model, test_loader, save='bow_test_top1.npy')
--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,81 @@ WORD_DIM = 300
 # with open('/Users/Weini/Dev/GloVe/glove.6B/word2vec_glove.6B.300d.pkl', 'wb') as f:
 #     pickle.dump(glove_model, f)
 
+
+
+
+def build_vocab(json_filename, glove_p_filename, save_vocab='./data/vqa_glove_vocab2ind.pkl', save_embeddings='./data/vqa_glove_embeddings.pkl'):
+	vocab = {'NULL': 0}
+	vecs = np.zeros(WORD_DIM)
+
+	with open(glove_p_filename,'rb') as f:
+		glove_embed = pickle.load(f)
+
+	with open(json_filename) as file:
+		json_data = json.load(file)
+		for img in json_data['images']:
+			for qapair in img['qa_pairs']:
+				# qa_id, image_id
+				li = []
+				li.extend(word_tokenize(qapair['question'].strip(string.punctuation).lower()))
+				# q = word_tokenize(qapair['question'].strip(string.punctuation).lower())
+				li.extend(word_tokenize(qapair['answer'].strip(string.punctuation).lower()))
+				# answers = [word_tokenize(qapair['answer'].strip(string.punctuation).lower())]
+				for choice in qapair['multiple_choices']:
+					# answers.append(word_tokenize(choice.strip(string.punctuation).lower()))
+					li.extend(word_tokenize(choice.strip(string.punctuation).lower()))
+
+				for t in li:
+					if t not in vocab:
+						if t in glove_embed:
+							# vecs.append(glove_embed[t])
+							vecs = np.vstack((vecs, glove_embed[t]))
+							vocab[t] = vecs.shape[0]-1
+
+	with open(save_vocab, 'wb') as handle:
+		pickle.dump(vocab, handle)
+	with open(save_embeddings, 'wb') as handle:
+		pickle.dump(vecs, handle)
+
+def process_qas_embeds(json_filename, vocab_p_filename, save_file=''):
+	with open(vocab_p_filename,'rb') as f:
+		vocab = pickle.load(f)
+
+	qa_map = []
+	with open(json_filename) as file:
+		json_data = json.load(file)
+		for img in json_data['images']:
+			for qapair in img['qa_pairs']:
+				# qa_id, image_id
+				q = word_tokenize(qapair['question'].strip(string.punctuation).lower())
+				answers = [word_tokenize(qapair['answer'].strip(string.punctuation).lower())]
+				for choice in qapair['multiple_choices']:
+					answers.append(word_tokenize(choice.strip(string.punctuation).lower()))
+
+				q_ids = []
+				as_ids = []
+				for t in q:
+					id = vocab[t] if t in vocab else 0
+					q_ids.append(id)
+				q_ids = np.asarray(q_ids, dtype=np.int32)
+				for a in answers:
+					a_ids = []
+					for t in a:
+						id = vocab[t] if t in vocab else 0
+						a_ids.append(id)
+					as_ids.append(np.asarray(a_ids, dtype=np.int32))
+
+				qa = {'qa_id': qapair['qa_id'],
+					  'img_id': img["image_id"],
+					  'q_embed': q_ids, # nparray of indices
+					  'a_embeds': as_ids} # list of 4 nparray of indices
+				qa_map.append(qa)
+				# print()
+		if save_file:
+			with open(save_file, 'wb') as handle:
+				pickle.dump(qa_map, handle)
+		return qa_map
+
 def sent2vec(glove_embed, sentence, word_dim):
 	v = np.array([]).reshape(0, word_dim)
 	for w in sentence:
@@ -45,20 +120,6 @@ def process_qas(json_filename, glove_p_filename, save=''):
 				for ans in answers:
 					av = sent2vec(glove_embed, ans, WORD_DIM)
 					av_list.append(av)
-					# together = np.vstack((qv, ansv))
-					# print("together", together.shape)
-					# qa_m = np.hstack((qa_m, together))	
-					# try:
-					# 	ansv = sent2vec(glove_embed, ans, WORD_DIM)
-					# 	together = np.vstack((qv, ansv))
-					# 	print("together", together.shape)
-					# 	qa_m = np.hstack((qa_m, together))
-					# except:
-					# 	print("error")
-					# 	print(ansv.shape)
-					# 	print(np.vstack((qv, ansv)).shape)
-					# 	print(ans)
-					# 	exit()
 				qa = {'qa_id': qapair['qa_id'],
 						'img_id': img["image_id"],
 						'q_embed': qv,
@@ -72,7 +133,13 @@ def process_qas(json_filename, glove_p_filename, save=''):
 if __name__ == '__main__':
 	json_filename = './data/visual7w-telling.json'
 	glove_p_filename = './data/word2vec_glove.6B.300d.pkl'
-	print(process_qas(json_filename, glove_p_filename)[0])
+	# print(process_qas(json_filename, glove_p_filename)[0])
+
+	build_vocab(json_filename, glove_p_filename)
+
+	process_qas_embeds('./data/visual7w-telling_train.json', './data/vqa_glove_vocab2ind.pkl', './data/qa_embeds_train.pkl')
+	process_qas_embeds('./data/visual7w-telling_val.json', './data/vqa_glove_vocab2ind.pkl', './data/qa_embeds_val.pkl')
+	process_qas_embeds('./data/visual7w-telling_test.json', './data/vqa_glove_vocab2ind.pkl', './data/qa_embeds_test.pkl')
 
 	# print('image_id_list len', len(image_id_list))
 	# print('image_id_list min', min(image_id_list))
