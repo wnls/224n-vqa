09014de85833f4a034334804406083c21e2a1a1b
ClaraBing <liubb16@connect.hku.hk>
2018-03-20 17:02:20 -0700

    multiple files

--- plot.py
+++ plot.py
@@ -11,7 +11,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--mode', default='best', type=str, help="'best' for outputing best results from all matching files; 'plot' for plotting curve for selected files.")
 parser.add_argument('--dir', default='checkpoints/')
 parser.add_argument('--format', default='*.json', type=str)
+parser.add_argument('--fig_format', default='', type=str)
 parser.add_argument('--attribute', default='val_acc', type=str)
+parser.add_argument('--multi', default='sep', type=str)
+# args for smoothing
+parser.add_argument('--window', default=20, type=int)
+parser.add_argument('--step', default=10, type=int)
 
 args = parser.parse_args()
 
@@ -22,35 +27,49 @@ def smooth(y, box_pts):
     y_smooth = np.convolve(y, box, mode='same')
     return y_smooth
 
+def plot(train_loss, train_acc, val_acc, figname='results.png'):
+	s = smooth(train_loss[::args.step], args.window)
+
+	fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(12, 4))
+	ax0.plot(s, 'g-', lw=1)
+	ax0.set_title("Train Loss")
+	# fig.autofmt_xdate()
+
+	print("Best train_acc: {}".format(max(train_acc)))
+	ax1.plot(train_acc)
+	ax1.set_title("Train Acc")
+	# fig.autofmt_xdate()
+
+	print("Best val_acc: {}".format(max(val_acc)))
+	ax2.plot(val_acc)
+	ax2.set_title("Val Acc")
+
+	plt.savefig(figname)
+
+
 files = sorted(glob(os.path.join(args.dir, args.format)))
 
-train_loss = []
-train_acc = []
-val_acc = []
+if args.multi == 'sep':
+# one plot per file
+	for fname in files:
+		with open(fname) as f:
+			data = json.load(f)
+			plot(data['train_loss'], data['train_acc'], data['val_acc'])
 
+elif args.multi == 'stitch':
 # stitch results from multiple files
-for fname in files:
-	with open(fname) as f:
-		data = json.load(f)
-		train_loss.extend(data['train_loss'])
-		train_acc.extend(data['train_acc'])
-		val_acc.extend(data['val_acc'])
+	train_loss = []
+	train_acc = []
+	val_acc = []
 
-s = smooth(train_loss[::10],20)
+	for fname in files:
+		with open(fname) as f:
+			data = json.load(f)
+			train_loss.extend(data['train_loss'])
+			train_acc.extend(data['train_acc'])
+			val_acc.extend(data['val_acc'])
+	plot(train_loss, train_acc, val_acc)
 
-fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(12, 4))
-ax0.plot(s, 'g-', lw=1)
-ax0.set_title("Train Loss")
-# fig.autofmt_xdate()
 
-print("Best train_acc: {}".format(max(train_acc)))
-ax1.plot(train_acc)
-ax1.set_title("Train Acc")
-# fig.autofmt_xdate()
 
-print("Best val_acc: {}".format(max(val_acc)))
-ax2.plot(val_acc)
-ax2.set_title("Val Acc")
-
-plt.savefig('test_three.png')
 
